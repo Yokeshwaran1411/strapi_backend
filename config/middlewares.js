805ee5a11c7dcc45ec576c5243c8c19bfedc67ae
Ykyokesh <yokeshwaran@giottus.com>
2026-03-27 12:06:25 +0530

    cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,18 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://res.cloudinary.com'],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
